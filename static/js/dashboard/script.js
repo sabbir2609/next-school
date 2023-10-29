fbeb71e6c1d257abf6784778945de8f171f0a31c
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-10-29 15:57:04 +0600

    dashboard updated

--- static/js/dashboard/script.js
+++ static/js/dashboard/script.js
@@ -19,3 +19,43 @@ toastElList.forEach(function (toastEl, index) {
     }, 10000)
 
 })
+
+
+// Sidebar toggle
+
+
+const sidebarToggle = document.getElementById('sidebar-toggle');
+sidebarToggle.addEventListener("click", function () {
+    document.getElementById("sidebar").classList.toggle("collapsed");
+});
+
+
+// theme toggle
+
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+    toggleLocalStorage();
+    toggleRootClass();
+});
+
+function toggleRootClass() {
+    const current = document.documentElement.getAttribute("data-bs-theme");
+    const inverted = current == "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-bs-theme", inverted);
+}
+
+function toggleLocalStorage() {
+    if (isLight()) {
+        localStorage.removeItem("light");
+    } else {
+        localStorage.setItem("light", "set");
+    }
+}
+
+function isLight() {
+    return localStorage.getItem("light");
+}
+
+if (isLight()) {
+    toggleRootClass();
+}
+
