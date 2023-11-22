741b2732d66d95a736c95197415c4228d53e1c7f
Sabbir <sabbirhasan2999@gmail.com>
2023-11-22 15:20:35 +0600

    .

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -22,10 +22,3 @@ DEBUG = False
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600),
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
