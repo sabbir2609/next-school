fc9e46c2a236d030259d61966a9d4746f36b449e
Sabbir <sabbirhasan2999@gmail.com>
2023-11-22 15:13:44 +0600

    .

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -19,13 +19,13 @@ DEBUG = False
 
 # If you are using a database other than sqlite, uncomment the following lines.
 
-# DATABASES = {
-#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600),
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600),
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
